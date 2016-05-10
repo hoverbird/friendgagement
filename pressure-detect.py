a5892fccda930cb6ab7412ea1b493193c90ee91c
patrick ewing <patrick.henry.ewing@gmail.com>
2016-05-10 05:09:31 +0000

    Only play sound if temple is currently inactive. Deactivate temple after 10 seconds

--- pressure-detect.py
+++ pressure-detect.py
@@ -2,17 +2,31 @@
 
 import os
 from time import sleep
+from threading import Timer
 
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
+
+templeActive = False
+
+def deactivateTemple():
+	print("Deactivating temple")
+	templeActive = False
+
+def activateTemple():
+	print("Deactivating temple")
+	templeActive = True
+
 
 while True:
-	print(GPIO.input(23))
-	if (GPIO.input(23) == True):
+	print(templeActive)
+	if (GPIO.input(23) == False and templeActive == False):	
+		activateTemple()
 		os.system('omxplayer -o local example.mp3 &')
 		# os.system('mpg123 -q example.mp3 &')
-	sleep(0.5);
+                Timer(10.0, deactivateTemple).start()
+
+	sleep(0.2);
 
